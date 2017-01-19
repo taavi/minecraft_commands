a69361cf5c749d4abb6713f133037c51cc8697e6
Taavi Burns <taavi@pagerduty.com>
2017-01-18 20:03:12 -0500

    Apparently signs despawn if they don't have all 4 lines of text?!

--- command_signs.py
+++ command_signs.py
@@ -7,7 +7,7 @@ def make_sign(x, y, z, text, action):
     >>> make_sign(-36, 67, -179, "bang" , "/tp @p -99 69 -149")
     /setblock -36 67 -179 wall_sign 5 replace {Text1:"{\"text\": \"bang\", \"clickEvent\": {\"action\": \"run_command\", \"value\": \"/tp @p -99 69 -149\"}}"}
     """
-    print "/setblock {x} {y} {z} wall_sign 5 replace {{Text1:{json}}}".format(
+    print "/setblock {x} {y} {z} wall_sign 5 replace {{Text1:{json},Text2:"",Text3:"",Text4:""}}".format(
         x=x, y=y, z=z, json=json.dumps(json.dumps({
             "text": text,
             "clickEvent": {
